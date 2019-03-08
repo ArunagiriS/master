102a2a506fbdf487653d76f87a299225057432c4
Arunagiri Sornamuthu <M1032045@mindtree.com>
2019-03-08 12:01:19 +0530

    Controller class created

---
+++ JenkinsDeployment/src/com/jenkins/controller/JenkinsController.java
@@ -0,0 +1,10 @@
+package com.jenkins.controller;
+
+public class JenkinsController {
+
+	public static void main(String[] args) {
+		System.out.println("Jenkins controller main method executed");
+
+	}
+
+}
