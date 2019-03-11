5b64e070a9469f434d7e2379bf9f76e60315c06c
Arunagiri Sornamuthu <M1032045@mindtree.com>
2019-03-11 15:02:53 +0530

    Update JenkinsController.java

--- JenkinsController.java
+++ JenkinsController.java
@@ -1,7 +1,7 @@
 public class JenkinsController {
 
 	public static void main(String[] args) {
-		System.out.println("Jenkins controller executed after a change");
+		System.out.println("Jenkins controller executed after code update");
 
 	}
 
